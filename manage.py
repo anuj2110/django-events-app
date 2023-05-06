d47dadd74da76e5f2fc3bc089867c447d5881133
anuj2110 <atrehan789@gmail.com>
2023-05-06 23:16:42 +0530

    started a new app members and moved templates to BASE_DIR

--- manage.py
+++ manage.py
@@ -2,12 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-
+from myclub_website.settings import BASE_DIR
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myclub_website.settings')
     try:
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
