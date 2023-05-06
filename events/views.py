99e2af6e259cb5dde704c74c0bb7a2f90c541e54
anuj2110 <atrehan789@gmail.com>
2023-05-06 11:52:35 +0530

    added update functionality for venue

--- events/views.py
+++ events/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
@@ -6,6 +6,14 @@ from .models import Events,Venue
 from .forms import VenueForm
 from django.http import HttpResponseRedirect
 # Create your views here.
+
+def update_venue(request,venue_id):
+    venue = Venue.objects.get(pk = venue_id)
+    form = VenueForm(request.POST or None,instance=venue)
+    if form.is_valid():
+        form.save()
+        return redirect('list-venues')
+    return render(request,'events/update-venue.html',{'form':form})
 
 def search_venues(request):
     if request.method=='POST':
@@ -15,12 +23,15 @@ def search_venues(request):
         return render(request,'events/search-venues.html',{'searched':searched,'venues':venues})
     else:
         return render(request,'events/search-venues.html',{})
+
 def show_venue(request,venue_id):
     venue = Venue.objects.get(id = venue_id)
     return render(request,'events/show-venue.html',{'venue':venue})
+
 def list_venues(request):
     venues = Venue.objects.all()
     return render(request,'events/list-venues.html',{'venues':venues})
+
 def add_venue(request):
     submitted = False
     if request.method=='POST':
@@ -34,9 +45,11 @@ def add_venue(request):
             submitted = True
         
     return render(request,'events/add_venue.html',{'form':form,'submitted':submitted})
+
 def list_events(request):
     events_list = Events.objects.all()
     return render(request,'events/list-events.html',{'events_list':events_list})
+
 def home(request, year = datetime.now().year, month = datetime.now().strftime('%B')):
     month_number = int(list(calendar.month_name).index(month.capitalize()))
     cal = HTMLCalendar().formatmonth(year,month_number)
